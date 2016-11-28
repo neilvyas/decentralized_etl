169b2c06a65eba058235c69215d4f59f55240704
Neil Vyas <vyas.neil.nv@gmail.com>
2016-11-28 16:35:34 -0600

    factoring out logline unpacking logic via arg inspection magic

--- src/decentralized_etl/EffPipeline.py
+++ src/decentralized_etl/EffPipeline.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from inspect import getargspec
 
 
 class EffPipeline:
@@ -14,7 +15,23 @@ class EffPipeline:
     def handle(cls, event_type):
         """Register an event handler for event_type.
 
-        Usage:
+        This method is a little magical but it leads to a very readable API,
+        so I'm ok with that.
+
+        Params
+        ------
+            event_type : string
+            Should be a member of decentralized_etl.EventTypes
+
+            handler : function (via decoration)
+            A handler function that has only positional arguments with names
+            that correspond to fields in the logline.
+
+            If the first parameter is 'state', then the handler is understood
+            to be 'stateful', and that parameter will be passed `self`.
+
+        Usage
+        -----
             @EffPipeline.handle('call')
             @EffPipeline.handle('put')
             def handle_option_exercise(...):
@@ -22,7 +39,26 @@ class EffPipeline:
         """
 
         def handler_registration(handler):
-            cls._handlers[event_type] = handler
+            handler_args = getargspec(handler).args
+
+            # Consider any handler with `state` or `self` as its first parameter to be stateful.
+            # TODO the way we handle stateful handlers at run time is just trying to pass both
+            # state and the logline and catching a TypeError if the handler doesn't need state.
+            # This is obviously bad because we check this condition for each handler and logline.
+            # Since we detect whether the handler is stateful here, we should be able to avoid this
+            # overhead at runtime.
+            if handler_args[0] in ('state', 'self'):
+                handler_args = handler_args[1:]
+
+            def handler_for_logline(logline):
+                # TODO Handle malformed loglines here.
+                assert all(arg in logline for arg in handler_args)
+
+                return handler(**{arg: logline[arg] for arg in handler_args})
+
+            cls._handlers[event_type] = handler_for_logline
+
+            # Return the original handler for testing purposes.
             return handler
 
         return handler_registration
@@ -49,6 +85,7 @@ class EffPipeline:
             # get _handlers from the instance in case user added additional handlers.
             handlers = self._handlers.get(event_type, [])
             for handler in handlers:
+                # FIXME see l:42
                 # Try to pass the handler some state, if it needs it, otherwise
                 # just pass in the logline.
                 try:
