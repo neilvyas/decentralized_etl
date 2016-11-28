cb3060a5f5057669c290f40bd129768a3af36b43
Neil Vyas <vyas.neil.nv@gmail.com>
2016-11-28 16:15:21 -0600

    switching event types to lower case because it's ~~modern~~

--- src/decentralized_etl/events.py
+++ src/decentralized_etl/events.py
@@ -1,8 +1,8 @@
 class EventTypes:
     """Encoding all the event types we want to handle.
     """
-    Declaration = 'Declaration'
-    Buy = 'Buy'
-    Sell = 'Sell'
-    Call = 'Call'
-    Put = 'Put'
+    Declaration = 'declaration'
+    Buy = 'buy'
+    Sell = 'sell'
+    Call = 'call'
+    Put = 'put'
